efa437ef4b4567ba00d6fc1f3152a4001979ba68
Onno Haldar <onno@zgw.nu>
2017-09-06 18:12:32 +0200

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule }           from '@angular/core'
 import { BrowserModule }      from '@angular/platform-browser'
-import { ZgwnuBonitaModule }  from '@zgwnu/ng-bonita'
 
 import { AppComponent }  from './app.component'
 
 @NgModule({
   imports: [ 
     BrowserModule,
-    ZgwnuBonitaModule, 
    ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]

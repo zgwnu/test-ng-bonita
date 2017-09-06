3263e0f512992454f8351c52caa1595f4d562e92
Onno Haldar <onno@zgw.nu>
2017-09-06 18:23:50 +0200

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core'
+import { ZgwnuBonitaAuthenticationService, ZgwnuBonitaCredentials } from '@zgwnu/ng-bonita'
 
 @Component({
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
+  providers: [
+    ZgwnuBonitaAuthenticationService, 
+  ]
 })
 export class AppComponent implements OnInit { 
 
-  public name = 'Angular' 
+  public name = 'Angular'
+
+  constructor(
+    private authenticationService: ZgwnuBonitaAuthenticationService,
+  )
+  {}
 
   ngOnInit() {
+    let creds: ZgwnuBonitaCredentials = new ZgwnuBonitaCredentials('onno.haldar', 'zgw')
+
     
   }
 

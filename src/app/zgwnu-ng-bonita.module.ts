07e93c959a7b858240b85ab9af806e40b58f447e
Onno Haldar <onno@zgw.nu>
2017-09-07 10:01:36 +0200

    refactor

--- src/app/zgwnu-ng-bonita.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { HttpModule } from '@angular/http'
-
-@NgModule({
-  imports: [CommonModule, HttpModule]
-})
-export class ZgwnuNgBonitaModule { }

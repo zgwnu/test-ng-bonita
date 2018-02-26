80f16a6b7576209052956c99157c9e3f5d882288
Onno Haldar <onno@zgw.nu>
2018-02-26 22:08:04 +0100

    authentication changes

--- src/app/ng-bonita-test/authentication-test/authentication-test.component.ts
+++ src/app/ng-bonita-test/authentication-test/authentication-test.component.ts
@@ -4,8 +4,8 @@ import { Component, OnInit } from '@angular/core'
 // RXJS Imports
 
 // ZGWNU Ng Bonita Module Imports
-import { ZgwnuBonitaConfigService, ZgwnuBonitaAuthenticationService, ZgwnuBonitaSessionService, 
-    ZgwnuBonitaCredentials, ZgwnuBonitaSession, ZgwnuBonitaResponse, ZgwnuBonitaErrorResponse 
+import { ZgwnuBonitaConfigService, ZgwnuBonitaAuthenticationService, ZgwnuBonitaCredentials, 
+  ZgwnuBonitaSession, ZgwnuBonitaErrorResponse
   } from '@zgwnu/ng-bonita'  
 
 // APP Imports
@@ -20,21 +20,16 @@ import { TestCaseService } from '../test/test-case.service'
 export class AuthenticationTestComponent implements OnInit { 
 
   // Process test vars
-  response: ZgwnuBonitaResponse
   session: ZgwnuBonitaSession
   errorResponse: ZgwnuBonitaErrorResponse
-  passedTest_SessionService_getSession: boolean = false
   passedTest_AuthenticationService_login: boolean = false
 
   constructor(
     private configService: ZgwnuBonitaConfigService,
-    private sessionService: ZgwnuBonitaSessionService,  
     private authenticationService: ZgwnuBonitaAuthenticationService,  
     public testCaseService: TestCaseService,  
   )
   {
-    // use @angular/cli proxy for local bonita rest api redirection
-    this.configService.bonitaUrls.hostUrl = 'http://localhost:4200'
   }
 
   ngOnInit() {
@@ -44,32 +39,17 @@ export class AuthenticationTestComponent implements OnInit {
   private test_AuthenticationService_login() {
     this.authenticationService.login(new ZgwnuBonitaCredentials('walter.bates', 'bpm'))
       .subscribe(
-        response => {
-          console.log(response)
-          this.response = response
-          this.passedTest_AuthenticationService_login = true
-          this.test_SessionService_GetSession()
-        },
-        errorResponse => {
-          console.log(errorResponse)
-          this.errorResponse = errorResponse
-        }
-      )
-  }
-
-  private test_SessionService_GetSession() {
-    this.sessionService.getSession()
-      .subscribe(
         session => {
-          console.log(session)
           this.session = session
-          this.passedTest_SessionService_getSession = true
-          console.log('this.configService.session', this.configService.session)
-          this.testCaseService.isAuthorized = true
         },
         errorResponse => {
           console.log(errorResponse)
           this.errorResponse = errorResponse
+        },
+        () => {
+          this.passedTest_AuthenticationService_login = true
+          this.testCaseService.isAuthorized = true
+          console.log('configService', this.configService)
         }
       )
   }

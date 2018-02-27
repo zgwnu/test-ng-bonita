dd6f2e6d598713aa5bee10253dd8c92d4a38b2cb
Onno Haldar <onno@zgw.nu>
2018-02-27 09:51:55 +0100

    refactor update packages

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,12 @@ describe('test-ng-bonita App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('ZGW NU Ng Bonita Test');
   });
+
+  it('should display session info', () => {
+    page.navigateTo();
+    expect(page.getAuthenticationSessionInfo()).toEqual('☺ Session');
+  });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,4 +8,8 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getAuthenticationSessionInfo() {
+    return element(by.css('authentication-test div h3')).getText();
+  }
 }
